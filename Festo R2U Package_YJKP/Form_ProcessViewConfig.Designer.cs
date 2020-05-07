b7f9a083c642970be5caffee1be2832c08910805
Tobby Li <tobby.li@festo.com>
2020-05-07 17:52:16 +0800

    0507

---
+++ Festo R2U Package_YJKP/Form_ProcessViewConfig.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Festo_R2U_Package_YJKP
+{
+    partial class Form_Init
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nUD_K = new System.Windows.Forms.NumericUpDown();
+            this.nUD_Continuity = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nUD_MinX_Value = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_K)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_Continuity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_MinX_Value)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 276F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Submit, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(816, 426);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WatchPath:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Program Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(313, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(466, 36);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Submit.Location = new System.Drawing.Point(313, 343);
+            this.btn_Submit.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(122, 42);
+            this.btn_Submit.TabIndex = 4;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox1.Location = new System.Drawing.Point(313, 86);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(384, 37);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(35, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(746, 198);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Params";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(740, 163);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 307F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 251F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel2.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.nUD_K, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.nUD_Continuity, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.nUD_MinX_Value, 2, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(740, 163);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "K Threshold";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(226, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Continuity Condition";
+            // 
+            // nUD_K
+            // 
+            this.nUD_K.DecimalPlaces = 2;
+            this.nUD_K.Location = new System.Drawing.Point(411, 3);
+            this.nUD_K.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nUD_K.Name = "nUD_K";
+            this.nUD_K.Size = new System.Drawing.Size(147, 36);
+            this.nUD_K.TabIndex = 2;
+            this.nUD_K.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // nUD_Continuity
+            // 
+            this.nUD_Continuity.Location = new System.Drawing.Point(411, 56);
+            this.nUD_Continuity.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nUD_Continuity.Name = "nUD_Continuity";
+            this.nUD_Continuity.Size = new System.Drawing.Size(147, 36);
+            this.nUD_Continuity.TabIndex = 3;
+            this.nUD_Continuity.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 29);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Min. X_Axis Value";
+            // 
+            // nUD_MinX_Value
+            // 
+            this.nUD_MinX_Value.DecimalPlaces = 2;
+            this.nUD_MinX_Value.Location = new System.Drawing.Point(411, 109);
+            this.nUD_MinX_Value.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.nUD_MinX_Value.Name = "nUD_MinX_Value";
+            this.nUD_MinX_Value.Size = new System.Drawing.Size(147, 36);
+            this.nUD_MinX_Value.TabIndex = 5;
+            this.nUD_MinX_Value.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // Form_Init
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 426);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("MetaPlusLF", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Init";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Init_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_K)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_Continuity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_MinX_Value)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nUD_K;
+        private System.Windows.Forms.NumericUpDown nUD_Continuity;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nUD_MinX_Value;
+    }
+}
+
---
+++ Festo R2U Package_YJKP/Form_ProcessViewConfig.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Festo_R2U_Package_YJKP
+{
+    public partial class Form_Init : Form
+    {
+        public Form_Init()
+        {
+            InitializeComponent();
+        }
+
+        public int DefaultWindowIndex
+        {
+            get
+            {
+                if (File.Exists("config.ini"))
+                {
+                    IniFile ini = new IniFile("config.ini");
+                    if (!ini.KeyExists("DefaultWindowIndex"))
+                    {
+                        return 0;
+                    }
+                    string s_max = ini.Read("DefaultWindowIndex");
+                    try
+                    {
+                        return Convert.ToInt16(s_max);
+                    }
+                    catch (Exception)
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                IniFile ini = new IniFile("config.ini");
+                ini.Write("DefaultWindowIndex", value.ToString());
+            }
+        }
+
+        public string WebLink
+        {
+            get
+            {
+                if (File.Exists("config.ini"))
+                {
+                    IniFile ini = new IniFile("config.ini");
+                    if (!ini.KeyExists("WebLink"))
+                    {
+                        return "";
+                    }
+                    string s_max = ini.Read("WebLink");
+                    try
+                    {
+                        return s_max;
+                    }
+                    catch (Exception)
+                    {
+                        return "";
+                    }
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            set
+            {
+                IniFile ini = new IniFile("config.ini");
+                ini.Write("WebLink", value.ToString());                
+            }
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            WebLink = textBox1.Text;
+            DefaultWindowIndex = Convert.ToInt16(comboBox1.SelectedItem.ToString());
+            switch (DefaultWindowIndex)
+            {
+                case 0:
+                    Form_Web mForm_Web = new Form_Web(WebLink);
+                    mForm_Web.Show();
+                    this.Hide();
+                    break;
+                case 1:
+                    new Form_Customized().Show();
+                    this.Hide();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form_Init_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = DefaultWindowIndex;
+            textBox1.Text = WebLink;
+        }
+    }
+}
